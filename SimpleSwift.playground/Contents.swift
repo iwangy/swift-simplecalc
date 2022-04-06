9fd90dd2bf236d6b327456bbb41b4649907af2e4
iwangy <35878784+iwangy@users.noreply.github.com>
2022-04-06 00:28:35 -0700

    complete calculate function p1

--- SimpleSwift.playground/Contents.swift
+++ SimpleSwift.playground/Contents.swift
@@ -1,11 +1,60 @@
+import Foundation
 print("Welcome to the UW Calculator Playground")
 
 func calculate(_ args: [String]) -> Int {
-    return -1
+    
+    switch args.last {
+        case "count":
+            return args.count - 1
+        case "avg":
+            if args.count - 1 == 0{
+                return 0
+            }
+            var sum : Int = 0
+            for index in 0...(args.count - 1){
+                sum += Int(args[index]) ?? 0
+            }
+            return sum / (args.count - 1)
+        case "fact":
+            var output = 1
+            if Int(args[0]) == 0 {
+                return 1
+            }
+            else if args.count - 1 == 0{
+                return 0
+            }
+            else {
+                for index in 1...Int(args[0])!{
+                    output *= index
+                }
+            }
+            return output
+            
+        default:
+            let num1 : Int = Int(args[0])!
+            let num2 : Int = Int(args[2])!
+            let operand = args[1]
+        
+            switch operand {
+                case "+":
+                    return num1 + num2
+                case "-":
+                    return num1 - num2
+                case "*":
+                    return num1 * num2
+                case "/":
+                    return num1 / num2
+                case "%":
+                    return num1 % num2
+                default:
+                    return -1
+            }
+    }
 }
 
+// check this
 func calculate(_ arg: String) -> Int {
-    return -1
+    return calculate(arg.components(separatedBy: " "))
 }
 
 // -------------------------------------------
@@ -22,27 +71,27 @@ calculate(["2", "%", "2"]) == 0
 calculate(["1", "2", "3", "count"]) == 3
 calculate(["1", "2", "3", "4", "5", "count"]) == 5
 calculate(["count"]) == 0
-
+//
 calculate(["1", "2", "3", "4", "5", "avg"]) == 3
-    // 15 / 5 = 3
+//    // 15 / 5 = 3
 calculate(["2", "2", "4", "4", "avg"]) == 3
-    // 12 / 4 = 3
+//    // 12 / 4 = 3
 calculate(["2", "avg"]) == 2
-    // 2 / 1 = 2
+//    // 2 / 1 = 2
 calculate(["avg"]) == 0
-    // 0 / 0 = 0 (not really, but it's an edge case
-
+//    // 0 / 0 = 0 (not really, but it's an edge case
+//
 calculate(["0", "fact"]) == 1
 calculate(["1", "fact"]) == 1
 calculate(["2", "fact"]) == 2 // 2*1
 calculate(["5", "fact"]) == 120 // 5*4*3*2*1
 calculate(["fact"]) == 0
-
+//
 calculate("2 + 2") == 4
 calculate("2 * 2") == 4
 calculate("2 - 2") == 0
 calculate("2 / 2") == 1
-
+//
 calculate("1 2 3 4 5 count") == 5
 calculate("1 2 3 4 5 avg") == 3
 calculate("5 fact") == 120
