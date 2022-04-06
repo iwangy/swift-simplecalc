936179ff47704320d4308de7ad4acbe772886820
iwangy <35878784+iwangy@users.noreply.github.com>
2022-04-06 15:31:40 -0700

    add import and format code

--- SimpleSwift.playground/Contents.swift
+++ SimpleSwift.playground/Contents.swift
@@ -71,15 +71,15 @@ calculate(["2", "%", "2"]) == 0
 calculate(["1", "2", "3", "count"]) == 3
 calculate(["1", "2", "3", "4", "5", "count"]) == 5
 calculate(["count"]) == 0
-//
+
 calculate(["1", "2", "3", "4", "5", "avg"]) == 3
-//    // 15 / 5 = 3
+// 15 / 5 = 3
 calculate(["2", "2", "4", "4", "avg"]) == 3
-//    // 12 / 4 = 3
+// 12 / 4 = 3
 calculate(["2", "avg"]) == 2
-//    // 2 / 1 = 2
+// 2 / 1 = 2
 calculate(["avg"]) == 0
-//    // 0 / 0 = 0 (not really, but it's an edge case
+// 0 / 0 = 0 (not really, but it's an edge case
 //
 calculate(["0", "fact"]) == 1
 calculate(["1", "fact"]) == 1
@@ -102,7 +102,7 @@ calculate("5 fact") == 120
 
 // Implement calculate([String]) and calculate(String)
 // to handle negative numbers
-/*
+
 calculate(["2", "+", "-2"]) == 0
 calculate(["2", "-", "-2"]) == 4
 calculate(["2", "*", "-2"]) == -4
@@ -117,16 +117,62 @@ calculate("2 - -2") == 4
 calculate("-2 / 2") == -1
 
 calculate("1 -2 3 -4 5 count") == 5
-*/
+
  
 // Implement calculate([String]) and calculate(String)
 // to use floating-point values
-/*
+
 func calculate(_ args: [String]) -> Double {
-    return -1.0
+    switch args.last {
+        case "count":
+        return Double(args.count - 1)
+        case "avg":
+            if args.count - 1 == 0{
+                return 0
+            }
+            var sum : Int = 0
+            for index in 0...(args.count - 1){
+                sum += Int(args[index]) ?? 0
+            }
+        return Double(sum / (args.count - 1))
+        case "fact":
+            var output = 1
+            if Int(args[0]) == 0 {
+                return 1
+            }
+            else if args.count - 1 == 0{
+                return 0
+            }
+            else {
+                for index in 1...Int(args[0])!{
+                    output *= index
+                }
+            }
+        return Double(output)
+            
+        default:
+            let num1 : Double = Double(args[0])!
+            let num2 : Double = Double(args[2])!
+            let operand = args[1]
+        
+            switch operand {
+                case "+":
+                    return Double(num1 + num2)
+                case "-":
+                    return Double(num1 - num2)
+                case "*":
+                    return Double(num1 * num2)
+                case "/":
+                    return Double(num1 / num2)
+                case "%":
+                    return Double(num1.truncatingRemainder(dividingBy: num2))
+                default:
+                    return -1.0
+            }
+    }
 }
 func calculate(_ arg: String) -> Double {
-    return -1.0
+    return calculate(arg.components(separatedBy: " "))
 }
 
 calculate(["2.0", "+", "2.0"]) == 4.0
@@ -136,4 +182,4 @@ calculate(["2.5", "*", "2.5"]) == 6.25
 calculate(["2.0", "/", "2.0"]) == 1.0
 calculate(["2.0", "%", "2.0"]) == 0.0
 calculate("1.0 2.0 3.0 4.0 5.0 count") == 5
-*/
+
